e22b2107c628d347e47f23635b01b07867eb3711
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-22 14:53:30 -0500

    feat: add Jasmine testing to project directory.

--- js/events.js
+++ js/events.js
@@ -7,16 +7,16 @@ let getMessageElem = document.getElementById('Message');
 
 getMessageElem.innerHTML = Message();
 
-//event listener that will add the class changing-font to the element with the id of 'Message'
-// document.getElementById('add-font-change').addEventListener('click', () => {
-//   getMessageElem.classList.add('changing-font');
-// })
+//add class on click event...
+$('#add-font-change').click(() => {
+  $('#Message').attr('class', 'changing-font');
+})
 
 //event listener that will remove the class changing-font to the element with the id of 'Message'
 document.getElementById('remove-font-change').addEventListener('click', () => {
   getMessageElem.removeAttribute('class', 'changing-font');
 })
 
-$('#add-font-change').click(() => {
-  $('#Message').attr('class', 'changing-font');
+$('#position-message').click(() => {
+  $('#Message').
 })
