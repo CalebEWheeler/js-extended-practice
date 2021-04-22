eba2c556bd5a356452cdd9ec0b867045b84e0544
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-22 10:53:07 -0500

    feat: import JQuery cdn to html file. Refactor method to add class to the element with the id of 'Message' to utilize JQuery selectors.

--- js/events.js
+++ js/events.js
@@ -8,11 +8,15 @@ let getMessageElem = document.getElementById('Message');
 getMessageElem.innerHTML = Message();
 
 //event listener that will add the class changing-font to the element with the id of 'Message'
-document.getElementById('add-font-change').addEventListener('click', () => {
-  getMessageElem.classList.add('changing-font');
-})
+// document.getElementById('add-font-change').addEventListener('click', () => {
+//   getMessageElem.classList.add('changing-font');
+// })
 
 //event listener that will remove the class changing-font to the element with the id of 'Message'
 document.getElementById('remove-font-change').addEventListener('click', () => {
   getMessageElem.removeAttribute('class', 'changing-font');
+})
+
+$('#add-font-change').click(() => {
+  $('#Message').attr('class', 'changing-font');
 })
