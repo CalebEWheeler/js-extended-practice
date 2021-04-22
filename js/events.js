06bf3329dbf8c1d9a256c7fce06cf3558235ce71
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-21 21:38:51 -0500

    feat: refactor by defining a variable to retrieve the element with the id of 'Message' to shorten the length of code written.

--- js/events.js
+++ js/events.js
@@ -3,16 +3,16 @@ const Message = () => {
   return "Welcome to the page where we will handle DOM events!";
 }
 
-document.getElementById('Message').innerHTML = Message();
+let getMessageElem = document.getElementById('Message');
+
+getMessageElem.innerHTML = Message();
 
 //event listener that will add the class changing-font to the element with the id of 'Message'
 document.getElementById('add-font-change').addEventListener('click', () => {
-  console.log("I'm clicked!");
-  document.getElementById('Message').classList.add('changing-font');
+  getMessageElem.classList.add('changing-font');
 })
 
 //event listener that will remove the class changing-font to the element with the id of 'Message'
 document.getElementById('remove-font-change').addEventListener('click', () => {
-  console.log("I'm clicked!");
-  document.getElementById('Message').removeAttribute('class', 'changing-font');
+  getMessageElem.removeAttribute('class', 'changing-font');
 })
