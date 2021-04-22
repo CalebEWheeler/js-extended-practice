d9afcd7c74ee6bebc0626302627d0c7787066df8
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-21 21:36:39 -0500

    feat: add css directory with a style.css file with a defined class name. Add two event listeners that will add and remove the class 'changing-font' when a user clicks on the designated button.

--- js/events.js
+++ js/events.js
@@ -4,3 +4,15 @@ const Message = () => {
 }
 
 document.getElementById('Message').innerHTML = Message();
+
+//event listener that will add the class changing-font to the element with the id of 'Message'
+document.getElementById('add-font-change').addEventListener('click', () => {
+  console.log("I'm clicked!");
+  document.getElementById('Message').classList.add('changing-font');
+})
+
+//event listener that will remove the class changing-font to the element with the id of 'Message'
+document.getElementById('remove-font-change').addEventListener('click', () => {
+  console.log("I'm clicked!");
+  document.getElementById('Message').removeAttribute('class', 'changing-font');
+})
