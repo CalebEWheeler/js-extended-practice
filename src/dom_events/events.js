a7f5d7a62c7dbb868d56dbf35c40b6251adc2e2f
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-05-05 21:03:26 -0500

    feat: create an event listener to log to the console the value a user puts into the search input when the user clicks on the search-submit button.

--- src/dom_events/events.js
+++ src/dom_events/events.js
@@ -1,7 +1,5 @@
 $(document).ready(() => {
 
-
-
   //Practice inserting a string value into an HTML element established a function with a value. Then I use jQuery's 'append()' function to append the output of the function 'Message()' to the element with an id of 'Message'.
   const Message = () => {
     return 'Welcome to the page where we will handle DOM events!';
@@ -27,7 +25,6 @@ $(document).ready(() => {
   let itemsInCart = 0;
     $('.add-to-cart').click(() => {
       //onclick add a value of 1 to the 'span' element with the parent element that has a class of cart and allow it to increment with each click. Also add the class of 'cart-count' if the value is 0.
-      console.log("I'm clicked!");
       itemsInCart++;
       $('#cart-count').attr('class', 'cart-count').text(itemsInCart);
     })
@@ -38,4 +35,7 @@ $(document).ready(() => {
     $('#cart-count').removeAttr('class', 'cart-count').text("");
   })
 
+  $(document).on('click', '#search-submit', () => {
+    console.log($('#search').val());
+  })
 })
